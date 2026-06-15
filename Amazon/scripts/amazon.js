c261d7e18e2b78c77f39859ac4da14f10aa5bcb3
Anton Matviiv <anton2263601@gmail.com>
2026-06-15 21:27:00 +0300

    Make the quantity selector interactive

--- Amazon/scripts/amazon.js
+++ Amazon/scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -58,6 +58,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
+    const selectedQuantity = document.querySelector(`.js-quantity-selector-${productId}`).value;
     let matchingItem;
 
     cart.forEach((item) => {
@@ -70,7 +71,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     else {
       cart.push({
         productId,
-        quantity: 1
+        quantity: Number(selectedQuantity)
       });
     }
 
