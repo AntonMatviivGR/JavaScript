1c5635153937401510acabf907fc75741f5ba362
Anton Matviiv <anton2263601@gmail.com>
2026-06-16 18:28:03 +0300

    Move addToCart into cart.js

--- Amazon/scripts/amazon.js
+++ Amazon/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = '';
@@ -58,34 +58,22 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   let timeoutID;
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
     const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
-    let matchingItem;
+    addToCart(productId, quantity);
+    updateCartQuantity()
 
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem) matchingItem.quantity += quantity;
-    else {
-      cart.push({
-        productId,
-        quantity
-      });
-    }
-
-    let cartQuantity = 0;
-
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     document.querySelector(`.js-added-to-cart-${productId}`).classList.add(`js-added-to-cart-visible`);
 
     clearTimeout(timeoutID);
