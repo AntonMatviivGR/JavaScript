fe8d445eb1c5a93d6d1989cc0e60b8859c58351a
Anton Matviiv <anton2263601@gmail.com>
2026-06-21 16:02:16 +0300

    Add new big tests

--- Amazon/data/cart.js
+++ Amazon/data/cart.js
@@ -1,3 +1,5 @@
+import {validDeliveryOption} from './deliveryOptions.js';
+
 export let cart;
 
 loadFromStorage();
@@ -76,6 +78,12 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
       matchingItem = cartItem;
     }
   });
+  if (!matchingItem) {
+    return;
+  }
+  if (!validDeliveryOption(deliveryOptionId)) {
+    return;
+  }
   matchingItem.deliveryOptionId = deliveryOptionId;
   saveToStorage();
 }
