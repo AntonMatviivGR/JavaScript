b054a40649922e2336f5b581de704ea2b7cc4586
Anton Matviiv <anton2263601@gmail.com>
2026-06-22 19:14:51 +0300

    Learn error handling

--- Amazon/data/products.js
+++ Amazon/data/products.js
@@ -64,6 +64,8 @@ export function loadProductsFetch() {
       return new Product(productDetails);
     });
     console.log('load products');
+  }).catch(() => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -86,6 +88,11 @@ export function loadProducts(fun) {
     console.log('load products');
     fun();
   });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
