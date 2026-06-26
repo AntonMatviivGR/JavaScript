183f1cc2dc44cffa2d8eee92642d63eb7b0eeda9
Anton Matviiv <anton2263601@gmail.com>
2026-06-26 16:26:59 +0300

    Ex 18r

--- Amazon/data/cart.js
+++ Amazon/data/cart.js
@@ -118,3 +118,8 @@ export async function loadCartFetch() {
   console.log(text);
   return text;
 }
+
+export function resetCart() {
+  cart = [];
+  saveToStorage();
+}
--- Amazon/scripts/tracking.js
+++ Amazon/scripts/tracking.js
@@ -19,13 +19,19 @@ async function loadPage() {
     }
   });
 
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+  const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+  const deliveredMessage = today < deliveryTime ? 'Arriving on' : 'Delivered on';
+
   const trackingHTML = `
     <a class="back-to-orders-link link-primary" href="orders.html">
       View all orders
     </a>
 
     <div class="delivery-date">
-      Arriving on ${
+      ${deliveredMessage} ${
         dayjs(productDetails.estimatedDeliveryTime).format('dddd, MMMM D')
       }
     </div>
@@ -41,19 +47,19 @@ async function loadPage() {
     <img class="product-image" src="${product.image}">
 
     <div class="progress-labels-container">
-      <div class="progress-label">
+      <div class="progress-label ${percentProgress < 50 ? 'current-status' : ''}">
         Preparing
       </div>
-      <div class="progress-label current-status">
+      <div class="progress-label ${(percentProgress >= 50 && percentProgress < 100) ? 'current-status' : ''}">
         Shipped
       </div>
-      <div class="progress-label">
+      <div class="progress-label ${percentProgress >= 100 ? "current-status" : ''}">
         Delivered
       </div>
     </div>
 
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+      <div class="progress-bar" style="width: ${percentProgress}%;"></div>
     </div>
   `;
 
